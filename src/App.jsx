2ffe2a009ce8c8a44d9d94913dec5edd2cf7005f
Evan Liu <140853704+evanliu-at-icstudents@users.noreply.github.com>
2024-12-15 21:35:58 -0600

    fixed an oopsy

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [mouseCookies, setMouseCookies] = useState(0); // Cookies gained from user clicks
   const [allCookies, setAllCookies] = useState(0); // Total cookies gained from all sources
   const [totalCPS, setTotalCPS] = useState(0);
-  const [baseClickValue, setBaseClickValue] = useState(1);
+  const [baseClickValue, setBaseClickValue] = useState(1000);
   const [cpcModifier, setCpcModifier] = useState(0);
   const [baseCursorCPS, setBaseCursorCPS] = useState(0.1);
   const [nonCursorGain, setNonCursorGain] = useState(0);
@@ -539,7 +539,7 @@ function App() {
               onMouseLeave={() => setHoverText('')}
               className="upgrade-button click-button blue"
             >
-              Buy {upgrade.name} ({upgrade.basePrice} cookies)
+              Buy {upgrade.name} ({shortNumbers(upgrade.basePrice)} cookies)
             </button>
           ))
         ) : (
@@ -560,7 +560,7 @@ function App() {
             onMouseLeave={() => setHoverText('')}
             className="building-button"
           >
-            Buy {key} ({Math.ceil(building.basePrice * building.multiplier ** building.count)} cookies) 
+            Buy {key} ({shortNumbers(Math.ceil(building.basePrice * building.multiplier ** building.count))} cookies) 
             - Owned: {building.count}
           </button>
         ))}
